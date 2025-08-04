83e349b5c8e8f7648ac388ffefa261805ccad3d6
Bo <gold.ro@northeastern.edu>
2025-08-04 16:13:04 -0500

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
 import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import Labs from "./Labs";
 import Kambaz from "./Kambaz";
-import Lab3 from "./Labs/Lab3";
 
 export default function App() {
   return (
     <HashRouter>
       <Routes>
-        {/* Redirect root to Labs */}
+        {/* Default to Kambaz dashboard */}
         <Route path="/" element={<Navigate to="/Kambaz" />} />
-        {/* Labs parent route */}
-        <Route path="/Labs/*" element={<Labs />} />
-        {/* Individual Lab route (optional) */}
-        <Route path="/labs/lab3" element={<Lab3 />} />
         {/* Main Kambaz app/dashboard route */}
         <Route path="/Kambaz/*" element={<Kambaz />} />
+        {/* All Labs under the Labs route (with nested labs routing) */}
+        <Route path="/Labs/*" element={<Labs />} />
       </Routes>
     </HashRouter>
   );
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,36 +1,45 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
+import Lab3 from "./Lab3";
 import Lab4 from "./Lab4";
-
 
 export default function Labs() {
   return (
     <div style={{ maxWidth: 600, margin: "2rem auto" }}>
       <h2>Labs – Bo Gold, Section 5160</h2>
       <ul>
-        <li><Link to="/labs/lab1">Lab 1: HTML/CSS</Link></li>
-        <li><Link to="/labs/lab2">Lab 2: CSS/Bootstrap</Link></li>
-        <li><Link to="/labs/lab3">Lab 3: JavaScript, Arrays, React</Link></li>
-        <li><Link to="/labs/lab4">Lab 4: State + Redux</Link></li>
-
+        {/* Use RELATIVE links here */}
+        <li><Link to="lab1">Lab 1: HTML/CSS</Link></li>
+        <li><Link to="lab2">Lab 2: CSS/Bootstrap</Link></li>
+        <li><Link to="lab3">Lab 3: JavaScript, Arrays, React</Link></li>
+        <li><Link to="lab4">Lab 4: State + Redux</Link></li>
       </ul>
 
+      {/* Always works */}
       <Link to="/Kambaz" style={{ marginRight: 16 }}>← Back to Kambaz App</Link>
 
       <div style={{ marginTop: 14 }}>
         <strong>Source Code Repos:</strong>
         <ul>
-          <li><a href="https://github.com/goldbo2002/kambaz" target="_blank"  rel="noopener noreferrer">Kambaz Repo</a></li>
+          <li>
+            <a
+              href="https://github.com/goldbo2002/kambaz"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Kambaz Repo
+            </a>
+          </li>
         </ul>
       </div>
 
-      {/* render cuz they werent working before */}
+      {/* Nested routes: Labs show up under the TOC, not as a new page */}
       <Routes>
         <Route path="lab1" element={<Lab1 />} />
         <Route path="lab2" element={<Lab2 />} />
+        <Route path="lab3" element={<Lab3 />} />
         <Route path="lab4" element={<Lab4 />} />
-
       </Routes>
     </div>
   );
