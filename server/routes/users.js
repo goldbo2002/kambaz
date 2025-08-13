09444fda06b06b25916a89547fcbea462f4bba72
Bo <gold.ro@northeastern.edu>
2025-08-13 16:20:36 -0500

    d

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,14 @@
-// SIGNUP
+
+const express = require("express");
+const User = require("../models/User");
+
+const router = express.Router();
+
+//sign up
 router.post("/signup", async (req, res, next) => {
   try {
     console.log("[SIGNUP] headers:", req.headers);
-console.log("[SIGNUP] body:", req.body);
+    console.log("[SIGNUP] body:", req.body);
 
     const { username, email, password, role, firstName, lastName } = req.body || {};
 
@@ -31,3 +37,58 @@ console.log("[SIGNUP] body:", req.body);
     return next(err);
   }
 });
+
+//sign in
+router.post("/signin", async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body || {};
+    if ((!username && !email) || !password) {
+      return res.status(400).json({ message: "username or email AND password are required" });
+    }
+
+    const query = username ? { username } : { email };
+    const user = await User.findOne(query);
+
+    if (!user || user.password !== password) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    req.session.user = {
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+    };
+
+    res.json(req.session.user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//signout
+router.post("/signout", (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) return next(err);
+    res.clearCookie("connect.sid", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
+    res.json({ message: "Signed out" });
+  });
+});
+
+
+router.get("/me", (req, res) => {
+  if (!req.session.user) {
+    return res.status(401).json({ message: "Not signed in" });
+  }
+  res.json(req.session.user);
+});
+
+router.get("/_version", (_req, res) => {
+  res.json({ signupRouteAcceptsEmail: true });
+});
+
+module.exports = router;
