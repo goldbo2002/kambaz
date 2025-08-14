ba37a543ebdba6ba6b586b00961a09605b6a1aef
Bo <gold.ro@northeastern.edu>
2025-08-14 10:52:29 -0500

    A

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: true,           
+    secure: process.env.NODE_ENV === 'production',           
     sameSite: "none",       //cross-site cookies
      httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24 * 7, 
