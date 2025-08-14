d79c0802792e2315ef2939343c272f2b62fa3cb0
Bo <gold.ro@northeastern.edu>
2025-08-14 02:00:45 -0500

    a

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ import Lab4 from "./Labs/Lab4";
 import Lab5 from "./Labs/Lab5";
 import Lab6 from "./Labs/Lab6";
 import People from "./Kambaz/People/People";
-const ParamsGuard = require("./Kambaz/Components/ParamsGuard").default;
+const ParamsGuard = require("./Kambaz/Components/ParamsGuard");
 function App() {
   return (
     <Routes>
