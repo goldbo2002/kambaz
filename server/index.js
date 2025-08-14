e2ce5c260e66e7de13b9b07732d8739651e02843
Bo <gold.ro@northeastern.edu>
2025-08-14 10:43:20 -0500

    t

--- server/index.js
+++ server/index.js
@@ -23,7 +23,8 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     secure: true,           
-    sameSite: "none",       //ross-site cookies
+    sameSite: "none",       //cross-site cookies
+     httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24 * 7, 
   },
 }));
